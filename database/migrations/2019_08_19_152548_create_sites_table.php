3af24c2e5a978a6cb86a8fa342571c26a67dad48
rsldgr <resuldeger2@gmail.com>
2019-08-19 19:28:35 +0300

    dil & tip & script % site tabloları ıluşturulduı

--- database/migrations/2019_08_19_152548_create_sites_table.php
+++ database/migrations/2019_08_19_152548_create_sites_table.php
@@ -38,39 +38,35 @@ class CreateSitesTable extends Migration
 
         Schema::create('sites', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('user_id');
-            $table->bigInteger('language_id');
-            $table->bigInteger('translate_id');
-            $table->bigInteger('type_id');
-            $table->bigInteger('script_id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('language_id')->unsigned();
+            $table->bigInteger('translate_id')->unsigned();
+            $table->bigInteger('type_id')->unsigned();
+            $table->bigInteger('script_id')->unsigned();
             $table->json('categories');	
             $table->json('options');	
             $table->text('editor_message');	
             $table->string('token', 80);
-            $table->unique('url');
+            $table->string('url')->unique();
             $table->date('expiration_date')->nullable();
             $table->timestamps();
 
             $table->foreign('user_id')
-                ->references('id')->on('users')
-                ->onDelete('cascade');
+                ->references('id')->on('users');
                 
             $table->foreign('type_id')
-                ->references('id')->on('sitetypes')
-                ->onDelete('cascade');
+                ->references('id')->on('sitetypes');
 
             $table->foreign('language_id')
-                ->references('id')->on('languages')
-                ->onDelete('cascade');
+                ->references('id')->on('languages');
 
             $table->foreign('translate_id')
-                ->references('id')->on('languages')
-                ->onDelete('cascade');
+                ->references('id')->on('languages');
 
             $table->foreign('script_id')
-                ->references('id')->on('scripts')
-                ->onDelete('cascade');
+                ->references('id')->on('scripts');
 
+            $table->unique('language_id', 'translate_id');
 
         });
 
@@ -84,10 +80,11 @@ class CreateSitesTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('sites');
         Schema::dropIfExists('languages');
         Schema::dropIfExists('scripts');
         Schema::dropIfExists('sitetypes');
-        Schema::dropIfExists('sites');
+        
 
     }
 }
